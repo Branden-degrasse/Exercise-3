6cb7e79ce447733d011fb9b621715c819f4dd951
Branden <branden.degrasse@uleth.ca>
2021-03-07 00:59:19 -0700

    THINGS ARE WORKING GOOD GOLY, it said variable was not defined for the longest time, managed to change the variable and for some reason it worked again

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,32 @@
 
 
 
-function Quote(quoteText, author, tags, color) {
-	this.quote = quoteText;
-	this.author = author;
+function Image(url, tags) {
+	this.url = url;
 	this.tags = tags;
-	this.color = color;
 	this.display = function() {
 
 		var container = $("<div>")
 		this.tags.forEach(function(tag){
-			container.addClass(tag)
+			container.addClass(tag);
 		})
-		container.css("background", this.color)
-		container.addClass("quote")
+		container.addClass("images")
 
-		var quoteString = "";
-		quoteString += "<p>" + this.quote + "</p>";
-		quoteString += "<cite>" + this.author + "</cite>";
+		var url = "";
+		url += "<img src="+ this.url + ">";
 
-		container.html(quoteString)
-		$(".quotes").prepend(container)
+		container.html(url)
+		$(".images").prepend(container)
 	}
 }
 
-var quotes = [
-	new Quote('"we don\'t make mistakes, just happy little accidents."', "Bob Ross", ["painting", "mistakes"], "#0a3410"),
-	new Quote('"Creativity takes courage"', "Henry Matisse", ["painting", "creativity"], "lightblue")
+var images = [
+	new Image("imgs/Draven.jpg", ["painting", "mistakes"]),
 ]
 
 //global taglist
 var tagList = []
-quotes.forEach(function(quote){
+images.forEach(function(quote){
 	quote.display();
 	quote.tags.forEach(function(tag){
 		// check to see if tag has been added to taglist
@@ -49,7 +44,7 @@ $(".filter").on("click", function(){
 	var tag = $(this).attr("id");
 	console.log(tag)
 
-	$(".quote").not("." + tag).hide();
+	$(".images").not("." + tag).hide();
 	$("." + tag).fadeIn();
 
 	$("filter").removeClass("active")
