98d0bfd42e0b6053b7f22c343ea9dacc2ff3acaa
Branden <branden.degrasse@uleth.ca>
2021-03-07 17:30:04 -0700

    added authors, images, and styled a bit

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 
 
 
-function Image(url, tags) {
+function Image(url, tags, author, title) {
 	this.urlimg = url;
 	this.tags = tags;
+	this.author = author;
+	this.title = title;
 	this.display = function() {
 
 		var container = $("<div>")
@@ -14,16 +16,28 @@ function Image(url, tags) {
 
 		var urlasdf = "";
 		urlasdf += "<img src="+ this.urlimg + ">";
+		urlasdf += "<p>" + this.title + "</p>";
+		urlasdf += "<cite>" + this.author + "</cite";
 
 		container.html(urlasdf)
+		console.log(urlasdf)
 		$(".imagesSkins").prepend(container)
 	}
 }
 
 var imagesSkins = [
-	new Image("imgs/Draven.jpg", ["Mecha Kingdoms", "Marksman"]),
-	new Image("imgs/Jax.jpg", ["Mecha Kingdoms", "Fighter"]),
-	new Image("imgs/Sett.jpg", ["Mecha Kingdoms", "Fighter"])
+	new Image("imgs/Draven.jpg", ["Mecha-Kingdoms", "Marksman", "All"], "Sean \"Raiko\" Tay", "Mecha Kingdoms Draven"),
+	new Image("imgs/Jax.jpg", ["Mecha-Kingdoms", "Fighter", "All"], "Sean \"Raiko\" Tay", "Mecha Kingdoms Jax"),
+	new Image("imgs/Sett.jpg", ["Mecha-Kingdoms", "Fighter", "All"], "Sean \"Raiko\" Tay", "Mecha Kingdoms Sett"),
+	new Image("imgs/Ezreal.jpg", ["Battle-Academia", "Marksman", "All"], "Chengwei Pan", "Battle Academia Ezreal"),
+	new Image("imgs/Graves.jpg", ["Battle-Academia", "Marksman", "All"], "Chengwei Pan", "Battle Academia Graves"),
+	new Image("imgs/Lux.jpg", ["Battle-Academia", "Mage", "All"], "Chengwei Pan", "Battle Academia Lux"),
+	new Image("imgs/Irelia.jpg", ["High-Noon", "Fighter", "All"], "Kelly Aleshire", "High Noon Irelia"),
+	new Image("imgs/Lucian.jpg", ["High-Noon", "Marksman", "All"], "Kelly Aleshire", "High Noon Lucian"),
+	new Image("imgs/Senna.jpg", ["High-Noon", "Marksman", "All"], "Kelly Aleshire", "High Noon Senna"),
+	new Image("imgs/Karma.jpg", ["Dark-Star", "Mage", "All"], "Bo Chen", "Dark Cosmic Karma"),
+	new Image("imgs/Jhin.jpg", ["Dark-Star", "Marksman", "All"], "Bo Chen", "Dark Star Jhin"),
+	new Image("imgs/Mord.jpg", ["Dark-Star", "Fighter", "All"], "Bo Chen", "Dark Star Mordekaiser"),
 ]
 
 //global taglist
